3468f1699d905a8b913dd291bfee79004802a059
Amrit Atwal <amritatwal04@gmail.com>
2022-05-28 18:40:56 +0100

    another test commit

--- database/scripts/phrases/populateTable.js
+++ database/scripts/phrases/populateTable.js
@@ -1,7 +1,6 @@
-import { query } from "../../connection.js";
+import { query } from "../../connection.js"; 
 
 const sqlString = `INSERT INTO phrases (phrase_desc, phrase_club) VALUES ($1, $2);`;
 
 const response = await query(sqlString, ["did you see the game last night?", "generic"]);
 
-console.log(response);
